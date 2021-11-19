d3b0b2e464e1fce70dc778bca78610019ad07d6a
QTHT9858 <OqE11@2s>
2021-11-19 10:15:55 +0100

    added blank payload without phone nodes

---
+++ thingin/payloads_no_phone.py
@@ -0,0 +1,32 @@
+initial_data_json = [
+    {
+      "_iri": "|inserted_domain|ecran",
+      "_domain": "|inserted_domain|",
+      "_labels": [
+        "Moniteur"
+      ],
+      "_visibility": 0,
+      "nom": "ecranPC",
+      "_outE": [
+        {
+          "_label": "estDans",
+          "_targetIRI": "|inserted_domain|salle_de_classe_insa"
+        },
+        {
+          "_label": "dispose_de",
+          "_targetIRI": "|inserted_domain|ecran.display"
+        }
+      ]
+    },
+    {
+      "_iri": "|inserted_domain|ecran.display",
+      "_domain": "|inserted_domain|",
+      "_labels": [
+        "Affichage"
+      ],
+      "_visibility": 0,
+      "nom": "ecranDisplay"
+    }
+]
+
+
